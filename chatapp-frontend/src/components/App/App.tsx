658361f77672b7d3b9d433c47228d9e9868f1e2f
omar hussain <omarhussain23moh@gmail.com>
2023-02-18 15:03:51 -0400

    create-react-app with typescript tailwind css initial folder structure

---
+++ chatapp-frontend/src/components/App/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+      </header>
+
+      <main>
+        <h1 className="text-3xl text-red-200 font-bold underline">
+          Hello world!
+        </h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
